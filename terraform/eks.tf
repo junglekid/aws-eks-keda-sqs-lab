2a25dff1d569f5e2a195462e3d4578fdf7af4e41
Dallin Rasmuson <junglekid@gmail.com>
2025-10-11 17:41:28 -0600

    Update tf code

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -42,6 +42,13 @@ module "eks" {
       max_size     = 20
       desired_size = 3
 
+      metadata_options = {
+        http_endpoint               = "enabled"
+        http_tokens                 = "required" # IMDSv2 only
+        http_put_response_hop_limit = 2          # REQUIRED for Pod Identity!
+        instance_metadata_tags      = "enabled"
+      }
+
       update_config = {
         max_unavailable = 1
       }
@@ -51,77 +58,6 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 }
-
-# # Create AWS EKS Node Group
-# resource "aws_eks_node_group" "eks" {
-#   cluster_name    = module.eks.cluster_name
-#   node_group_name = "node_workers"
-#   node_role_arn   = aws_iam_role.eks_node.arn
-#   subnet_ids      = module.vpc.private_subnets
-#   instance_types  = ["m6a.large", "m6i.large"]
-#   capacity_type   = "SPOT"
-
-#   scaling_config {
-#     desired_size = 3
-#     max_size     = 20
-#     min_size     = 3
-#   }
-
-#   update_config {
-#     max_unavailable = 1
-#   }
-# }
-
-# # Create AWS IAM Role for EKS Nodes
-# resource "aws_iam_role" "eks_node" {
-#   name = "${local.eks_iam_role_prefix}-node-group-role"
-
-#   assume_role_policy = jsonencode({
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#     Version = "2012-10-17"
-#   })
-# }
-
-# # Attach AWS IAM Policy to IAM Role for EKS Nodes
-# resource "aws_iam_role_policy_attachment" "eks_node-AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.eks_node.name
-# }
-
-# # Attach AWS IAM Policy to IAM Role for EKS Nodes
-# resource "aws_iam_role_policy_attachment" "eks_node-AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.eks_node.name
-# }
-
-# # Attach AWS IAM Policy to IAM Role for EKS Nodes
-# resource "aws_iam_role_policy_attachment" "eks_node-AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.eks_node.name
-# }
-
-# Create IAM Role for AWS VPC CNI Service Account
-# module "vpc_cni_ipv4_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name                  = "${local.eks_iam_role_prefix}-vpc-cni-ipv4"
-#   attach_vpc_cni_policy = true
-#   vpc_cni_enable_ipv4   = true
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:aws-node"]
-#     }
-#   }
-# }
 
 module "vpc_cni_ipv4_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
--- terraform/eks_roles.tf
+++ terraform/eks_roles.tf
@@ -1,19 +1,4 @@
 # Create IRSA Role for AWS ALB Service Account
-# module "load_balancer_controller_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name                                   = "${local.eks_iam_role_prefix}-lb-controller"
-#   attach_load_balancer_controller_policy = true
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-#     }
-#   }
-# }
-
 module "load_balancer_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 2.0"
@@ -32,22 +17,6 @@ module "load_balancer_eks_pod_identity" {
 }
 
 # Create IRSA Role for Cluster Autoscaler
-# module "cluster_autoscaler_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name                             = "${local.eks_iam_role_prefix}-cluster-autoscaler"
-#   attach_cluster_autoscaler_policy = true
-#   cluster_autoscaler_cluster_names = [module.eks.cluster_name]
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:cluster-autoscaler-aws-cluster-autoscaler"]
-#     }
-#   }
-# }
-
 module "cluster_autoscaler_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 2.0"
@@ -56,24 +25,17 @@ module "cluster_autoscaler_eks_pod_identity" {
 
   attach_cluster_autoscaler_policy = true
   cluster_autoscaler_cluster_names = [module.eks.cluster_name]
+
+  associations = {
+    cluster-autoscaler = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "kube-system"
+      service_account = "cluster-autoscaler-aws-cluster-autoscaler"
+    }
+  }
 }
 
 # Create IRSA Role for External DNS
-# module "external_dns_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name                       = "${local.eks_iam_role_prefix}-external-dns"
-#   attach_external_dns_policy = true
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:external-dns"]
-#     }
-#   }
-# }
-
 module "external_dns_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 2.0"
@@ -93,27 +55,6 @@ module "external_dns_eks_pod_identity" {
 }
 
 # Create IRSA Role for SQS
-# module "sqs_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name = "${local.eks_iam_role_prefix}-sqs"
-#   policies = {
-#     policy = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${local.sqs_name}-policy",
-#   }
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["sqs-app:${local.eks_sqs_app_service_account_name}"]
-#     }
-#   }
-
-#   depends_on = [
-#     aws_iam_policy.sqs
-#   ]
-# }
-
 module "sqs_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 2.0"
@@ -136,34 +77,9 @@ module "sqs_eks_pod_identity" {
       service_account = local.eks_sqs_app_service_account_name
     }
   }
-
-  # depends_on = [
-  #   aws_iam_policy.sqs
-  # ]
 }
 
 # Create IRSA Role for SQS Keda
-# module "sqs_keda_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-#   version = "~> 6.0"
-
-#   name = "${local.eks_iam_role_prefix}-sqs-keda"
-#   policies = {
-#     policy = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${local.sqs_name}-keda-policy",
-#   }
-
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["keda:keda-operator"]
-#     }
-#   }
-
-#   depends_on = [
-#     aws_iam_policy.sqs-keda
-#   ]
-# }
-
 module "sqs_keda_eks_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 2.0"
@@ -186,8 +102,4 @@ module "sqs_keda_eks_pod_identity" {
       service_account = "keda-operator"
     }
   }
-
-  # depends_on = [
-  #   aws_iam_policy.sqs-keda
-  # ]
 }
